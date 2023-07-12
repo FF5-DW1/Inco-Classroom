9f163b7da50d8ce41d1001cc5a8fb26f74b17341
Olga G <oldchatot@gmail.com>
2023-07-12 16:13:13 +0200

    Imágenes, eliminación del botón de registro

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,9 +7,6 @@
             <h1>Welcome</h1>
             <p>Disfruta de nuestros cursos con los mejores profesionales del sector</p>
             <x-signinbutton />
-
-            <a href="{{ route('register') }}"><button class="botonAzul">REGISTRO</button></a>
-
             <button class="botonBlanco">GOOGLE AUTH</button>
         </div>
     </div>
